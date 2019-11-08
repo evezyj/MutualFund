93cc7235a65cc50a74fe224661e7acbca0c57976
Eve_zyj <evezyj0102@163.com>
2019-11-09 00:19:56 +0800

    dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BuyComponentComponent } from './buy-component/buy-component.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuyComponentComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
